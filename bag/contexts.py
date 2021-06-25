9932c92fe2b0bee445ac45bb55c0029f34707af2
niall-mulcahy <niall-mulcahy@outlook.com>
2021-06-25 12:39:26 +0000

    update and delete shopping bag finished

--- bag/contexts.py
+++ bag/contexts.py
@@ -5,7 +5,6 @@ from products.models import Product
 
 
 def bag_contents(request):
-    
     bag_items = []
     total = 0
     product_count = 0
@@ -19,7 +18,7 @@ def bag_contents(request):
             bag_items.append({
                 'item_id': item_id,
                 'quantity': item_data,
-                'product': product, 
+                'product': product,
             })
         else:
             product = get_object_or_404(Product, pk=item_id)
@@ -34,14 +33,14 @@ def bag_contents(request):
                 })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
         free_delivery_delta = 0
-
+    
     grand_total = delivery + total
-
+    
     context = {
         'bag_items': bag_items,
         'total': total,
