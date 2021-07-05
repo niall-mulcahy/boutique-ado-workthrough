4276f9ba199311e0217b982bc2b99917aa42209d
niall-mulcahy <niall-mulcahy@outlook.com>
2021-07-05 11:24:41 +0000

    stripe not working

--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+import stripe
 
 from .forms import OrderForm
 
@@ -14,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51J9omdJq4HTAYf7lueZYAvtrZobGmqSRbBHLdQHtOLAKNn86VA2lNidxBAKf092yMIL4jVvUPdigVYUg18OxRSae00TDu9dySF',
+        'client_secret': 'sk_test_51J9omdJq4HTAYf7lMP4GrPqDjsG4lXcfuh1CQ9ZIX45P5ACsfBNEoIs7XA8U7GX8ZIpGFAkjhnNjORPmfj6sKHhG00Lu3Goq8k',
     }
 
     return render(request, template, context)
